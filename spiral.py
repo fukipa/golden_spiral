03d886fdedb0540428b9428c49ac6675910e3d9f
fukipa <30544790+fukipa@users.noreply.github.com>
2022-01-12 10:50:57 +0200

    Works

---
+++ spiral.py
@@ -0,0 +1,15 @@
+from math import *
+import matplotlib.pyplot as plt
+n=4
+G=(1+5**.5)/2
+print('Golden ratio =', G, 'Number of quarters =' , n)
+w=250
+print('Width =', w)
+k=pi/180
+pt =[(G**(j/90)*cos(j*k)+w/2,G**(j/90)*sin(j*k)+w/2)for j in range(n*90)]
+#print(pt) # the (x,y) tuple
+plt.scatter (*zip(*pt)) # plot the tuples
+plt.show()
+#d.line([(G**(j/90)*cos(j*k)+w/2,G**(j/90)*sin(j*k)+w/2)for j in range(n*90)],fill=0)
+#i.save("s.png","PNG")
+
